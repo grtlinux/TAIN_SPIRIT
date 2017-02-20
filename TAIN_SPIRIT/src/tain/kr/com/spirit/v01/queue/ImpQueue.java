9a0269bcc22c35c5fdf9cda0f4af42612607b5eb
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-21 06:09:02 +0900

    .

---
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v01/queue/ImpQueue.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.spirit.v01.queue;
+
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : ImpQueue.java
+ *   -. Package    : tain.kr.com.spirit.v01.queue
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 21. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public interface ImpQueue {
+
+	public abstract int put(Object object) throws Exception;
+	public abstract Object get() throws Exception;
+	public abstract Object get(long timeout) throws Exception;
+	public abstract void clear();
+	public abstract int getSize();
+}

420e1db41858c6b9c56245ebfedc74e27c7600ab
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-25 18:47:06 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/com/spirit/v02/data/DataContent.java
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v02/data/DataContent.java
@@ -19,6 +19,11 @@
  */
 package tain.kr.com.spirit.v02.data;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -35,27 +40,113 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class DataContent {
+public final class DataContent extends AbsData {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(DataContent.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String TYP_CHARSET = "euc-kr";
+	
+	private String strData;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public DataContent() {
-		if (flag)
+	public DataContent(int size) {
+		
+		super(size);
+		
+		if (!flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
+	public DataContent() {
+		this(AbsData.SIZ_DEF_BYTDATA);
+	}
+	
+	public DataContent(String strData) throws IOException {
+		this();
+		
+		setStrData(strData);
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public void setStrData(String strData) throws IOException {
+		this.strData = strData;
+		
+		byte[] bytSrc = this.strData.getBytes(Charset.forName(TYP_CHARSET));
+		this.size = bytSrc.length;
+		
+		if (this.bytData.length < this.size) {
+			throw new IOException(String.format("[ERROR] size (%d) is more than byte buffer 'bytData' (%d)"
+					, this.size, this.bytData.length));
+		}
+		
+		System.arraycopy(bytSrc, 0, this.bytData, 0, this.size);
+	}
+	
+	public void setStrData() {
+		this.strData = new String(this.bytData, 0, this.size, Charset.forName(TYP_CHARSET));
+	}
+	
+	public String getStrData() {
+		return this.strData;
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/* (non-Javadoc)
+	 * @see tain.kr.com.spirit.v02.data.AbsData#readFromInputStream(java.io.InputStream)
+	 */
+	@Override
+	public int readFromInputStream(InputStream is) throws IOException {
+		
+		this.size = is.read(this.bytData);
+		
+		if (this.size < 0) {
+			throw new IOException(String.format("[STATUS] read EOF from InputStream.."));
+		}
+		
+		if (!flag) setStrData();
+		
+		return this.size;
+	}
+
+	/* (non-Javadoc)
+	 * @see tain.kr.com.spirit.v02.data.AbsData#writeToOutputStream(java.io.OutputStream)
+	 */
+	@Override
+	public void writeToOutputStream(OutputStream os) throws IOException {
+		
+		os.write(this.bytData, 0, this.size);
+		os.flush();
+	}
+
+	/* (non-Javadoc)
+	 * @see tain.kr.com.spirit.v02.data.AbsData#createClone()
+	 */
+	@Override
+	public DataContent createClone() {
+		
+		DataContent content = null;
+		
+		try {
+			content = (DataContent) clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		
+		return content;
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////

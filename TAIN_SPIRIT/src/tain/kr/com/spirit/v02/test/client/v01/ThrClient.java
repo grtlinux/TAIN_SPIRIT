797837e3e1b851c166571c6b52aa60b78535a3b1
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-25 20:13:09 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/com/spirit/v02/test/client/v01/ThrClient.java
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v02/test/client/v01/ThrClient.java
@@ -19,7 +19,15 @@
  */
 package tain.kr.com.spirit.v02.test.client.v01;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
 import org.apache.log4j.Logger;
+
+import tain.kr.com.spirit.v02.loop.LoopSleep;
 
 /**
  * Code Templates > Comments > Types
@@ -42,18 +50,127 @@ public final class ThrClient extends Thread {
 	private static final Logger log = Logger.getLogger(ThrClient.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String TYP_CHARSET = "euc-kr";
+
+	private static final String CONNECT_HOST = "127.0.0.1";
+	private static final String CONNECT_PORT = "20025";
+	
+	private final Socket socket;
+	private final DataInputStream dis;
+	private final DataOutputStream dos;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public ThrClient() {
+	public ThrClient() throws Exception {
+		
+		super("MAIN_CLIENT");
+		
+		this.socket = new Socket(CONNECT_HOST, Integer.parseInt(CONNECT_PORT));
+		if (this.socket == null) {
+			throw new IOException("ERROR: socket is null pointers..");
+		}
+		this.dis = new DataInputStream(this.socket.getInputStream());
+		this.dos = new DataOutputStream(this.socket.getOutputStream());
+		
 		if (flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private String strSend;
+	private byte[] bytSend;
+	private int nSend;
+	
+	private String strRecv;
+	private byte[] bytRecv;
+	private int nRecv;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void run() {
+		
+		if (flag) {
+			for (int i=0; i < 1; i++) {
+				if (flag) {
+					/*
+					 * send
+					 */
+					this.strSend = "Hello, world!!!";
+					this.bytSend = this.strSend.getBytes(Charset.forName(TYP_CHARSET));
+					this.nSend = this.bytSend.length;
+					
+					try {
+						this.dos.write(this.bytSend, 0, this.nSend);
+					} catch (IOException e) {
+						e.printStackTrace();
+						break;
+					}
+					
+					if (flag) System.out.printf("%s [STATUS] SEND [%d:%s]\n"
+							, Thread.currentThread().getName(), this.nSend, this.strSend);
+				}
+				
+				if (flag) {
+					/*
+					 * recv
+					 */
+					this.nRecv = 1024;
+					this.bytRecv = new byte[nRecv];
+					
+					try {
+						this.nRecv = this.dis.read(bytRecv, 0, nRecv);
+						if (this.nRecv == 0) {
+							/*
+							 * read data of 0 size
+							 */
+							if (flag) System.out.printf("%s [STATUS] read data of 0 size..\n", Thread.currentThread().getName());
+						} else if (this.nRecv < 0) {
+							/*
+							 * EOF
+							 */
+							if (flag) System.out.printf("%s [STATUS] read data of EOF...\n", Thread.currentThread().getName());
+							break;
+						}
+					} catch (IOException e) {
+						/*
+						 * Exception
+						 */
+						if (flag) System.out.printf("%s [STATUS] exception during reading...\n", Thread.currentThread().getName());
+						e.printStackTrace();
+						break;
+					}
+					
+					this.strRecv = new String(this.bytRecv, 0, this.nRecv, Charset.forName(TYP_CHARSET));
+					
+					if (flag) System.out.printf("%s [STATUS] RECV [%d:%s]\n"
+							, Thread.currentThread().getName(), this.nRecv, this.strRecv);
+				}
+				
+				if (flag) {
+					/*
+					 * sleep
+					 */
+					LoopSleep.sleep(1 * 1000);
+				}
+			}
+			
+			if (flag) {
+				/*
+				 * close
+				 */
+				if (this.dos != null) try { this.dos.close(); } catch (IOException e) {}
+				if (this.dis != null) try { this.dis.close(); } catch (IOException e) {}
+				if (this.socket != null) try { this.socket.close(); } catch (IOException e) {}
+			}
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,9 +186,6 @@ public final class ThrClient extends Thread {
 	 * static test method
 	 */
 	private static void test01(String[] args) throws Exception {
-
-		if (flag)
-			new ThrClient();
 
 		if (flag) {
 

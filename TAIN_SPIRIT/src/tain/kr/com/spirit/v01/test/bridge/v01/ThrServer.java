7533971e28f3802f1607e586df56f9f12ac6d065
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-25 11:21:48 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/com/spirit/v01/test/bridge/v01/ThrServer.java
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v01/test/bridge/v01/ThrServer.java
@@ -19,6 +19,9 @@
  */
 package tain.kr.com.spirit.v01.test.bridge.v01;
 
+import java.io.IOException;
+import java.net.Socket;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -42,18 +45,89 @@ public final class ThrServer extends Thread {
 	private static final Logger log = Logger.getLogger(ThrServer.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String CONNECT_HOST = "192.168.0.15";
+	private static final String CONNECT_PORT = "3389";
+	
+	private int idx;
+	private Socket socket1;
+	private Socket socket2;
+	
+	public boolean flagStopThread = false;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public ThrServer() {
+	public ThrServer(int idx, Socket socket1) throws Exception {
+		
+		super(String.format("JOINT_%03d", idx));
+		
+		this.idx = idx;
+		this.socket1 = socket1;
+		this.socket2 = new Socket(CONNECT_HOST, Integer.parseInt(CONNECT_PORT));
+		
+		if (this.socket1 == null || this.socket2 == null) {
+			throw new Exception("socket variables is null(tain.co.kr)");
+		}
+		
 		if (flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void run() {
+		
+		ThrRecvSend thread1 = null;
+		ThrRecvSend thread2 = null;
+		
+		if (flag) {
+			/*
+			 * create thread
+			 */
+			try {
+				thread1 = new ThrRecvSend(String.format("JOINT_%03d_1", this.idx), this
+						, this.socket1.getInputStream(), this.socket2.getOutputStream());
+				
+				thread2 = new ThrRecvSend(String.format("JOINT_%03d_2", this.idx), this
+						, this.socket2.getInputStream(), this.socket1.getOutputStream());
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+		
+		if (flag) {
+			/*
+			 * start thread
+			 */
+			thread1.start();
+			thread2.start();
+		}
+		
+		if (flag) {
+			/*
+			 * join thread
+			 */
+			try {
+				thread1.join();
+				thread2.join();
+			} catch (InterruptedException e) {}
+		}
+		
+		if (flag) {
+			/*
+			 * close
+			 */
+			if (this.socket1 != null) try { this.socket1.close(); } catch (IOException e) {}
+			if (this.socket2 != null) try { this.socket2.close(); } catch (IOException e) {}
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,9 +143,6 @@ public final class ThrServer extends Thread {
 	 * static test method
 	 */
 	private static void test01(String[] args) throws Exception {
-
-		if (flag)
-			new ThrServer();
 
 		if (flag) {
 

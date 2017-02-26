c6e58ebffa246c5c3a4f282aa61fff9b72ae28ea
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-27 00:08:08 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/runjar/v03/JIJConstants.java
+++ TAIN_SPIRIT/src/tain/kr/runjar/v03/JIJConstants.java
@@ -35,13 +35,30 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class JIJConstants {
+final class JIJConstants {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(JIJConstants.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	static final String REDIRECTED_MAIN_CLASS_MANIFEST_NAME  = "Rsrc-Main-Class";
+	static final String REDIRECTED_CLASS_PATH_MANIFEST_NAME  = "Rsrc-Class-Path";
+	static final String DEFAULT_REDIRECTED_CLASSPATH         = "";
+	
+	static final String UTF8_ENCODING                        = "UTF-8";
+	
+	static final String PATH_SEPARATOR                       = "/";
+	static final String CURRENT_DIR                          = "./";
+
+	static final String INTERNAL_URL_PROTOCOL                = "rsrc";
+	static final String INTERNAL_URL_PROTOCOL_WITH_COLON     = "rsrc:";
+	
+	static final String JAR_INTERNAL_URL_PROTOCOL_WITH_COLON = "jar:rsrc:";
+	static final String JAR_INTERNAL_SEPARATOR               = "!/";
+	static final String MAIN_METHOD_NAME                     = "main";
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*

7e0f80426008a7d271eafd5dd9584efb0fb80cd1
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-27 13:04:25 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/com/spirit/v03/control/ThrControlServer.java
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v03/control/ThrControlServer.java
@@ -19,7 +19,17 @@
  */
 package tain.kr.com.spirit.v03.control;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
 import org.apache.log4j.Logger;
+
+import tain.kr.com.spirit.v03.loop.LoopSleep;
+import tain.kr.com.spirit.v03.main.MainControlServer;
 
 /**
  * Code Templates > Comments > Types
@@ -35,26 +45,112 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class ThrControlServer {
+public final class ThrControlServer extends Thread {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(ThrControlServer.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static final String THREAD_NAME = "CONTROL_SERVER";
+	
+	private final MainControlServer mainControlServer;
+	
+	private final Socket socket;
+	private final DataInputStream dis;
+	private final DataOutputStream dos;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public ThrControlServer() {
+	public ThrControlServer(MainControlServer mainControlServer, ServerSocket controlServerSocket) throws Exception {
+		
+		super(THREAD_NAME);
+		
+		this.mainControlServer = mainControlServer;
+		this.socket = controlServerSocket.accept();
+		if (this.socket == null) {
+			throw new IOException("ERROR: socket is null pointer...");
+		}
+		this.dis = new DataInputStream(this.socket.getInputStream());
+		this.dos = new DataOutputStream(this.socket.getOutputStream());
+		
 		if (flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String TYP_CHARSET = "euc-kr";
+
+	private String strSend;
+	private byte[] bytSend;
+	private int nSend;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void run() {
+		
+		if (flag) {
+			/*
+			 * start thread process : server
+			 * send
+			 */
+			try {
+				while (true) {
+					
+					if (flag) {
+						/*
+						 * get from queue
+						 */
+						this.strSend = (String) this.mainControlServer.getQueue().get();
+					}
+					
+					if (flag) {
+						/*
+						 * send data to client
+						 */
+						this.bytSend = this.strSend.getBytes(Charset.forName(TYP_CHARSET));
+						this.nSend = this.bytSend.length;
+						
+						try {
+							this.dos.write(this.bytSend, 0, this.nSend);
+						} catch (IOException e) {
+							throw e;
+						}
+						
+						if (flag) System.out.printf("%s [SEND] [%d:%s]\n", Thread.currentThread().getName(), this.nSend, this.strSend);
+					} // end of send
+					
+					if (flag) {
+						/*
+						 * sleep
+						 */
+						LoopSleep.sleep(10 * 500);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (flag) {
+					/*
+					 * close
+					 */
+					if (this.dos != null) try { this.dos.close(); } catch (IOException e) {}
+					if (this.dis != null) try { this.dis.close(); } catch (IOException e) {}
+					if (this.socket != null) try { this.socket.close(); } catch (IOException e) {}
+				}
+			}
+			
+			if (flag) System.out.printf("%s >>>>> END OF THREAD\n", Thread.currentThread().getName());
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,9 +165,6 @@ public class ThrControlServer {
 	 * static test method
 	 */
 	private static void test01(String[] args) throws Exception {
-
-		if (flag)
-			new ThrControlServer();
 
 		if (flag) {
 

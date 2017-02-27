bebd7fa52bae31d667abe1098088b92d168eb6e2
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-27 11:36:33 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/com/spirit/v03/recvsend/ThrRecvSend.java
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v03/recvsend/ThrRecvSend.java
@@ -19,7 +19,18 @@
  */
 package tain.kr.com.spirit.v03.recvsend;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.SocketTimeoutException;
+
 import org.apache.log4j.Logger;
+
+import tain.kr.com.spirit.v03.joint.AbsJoint;
+import tain.kr.com.spirit.v03.loop.LoopSleep;
+
 
 /**
  * Code Templates > Comments > Types
@@ -42,19 +53,132 @@ public final class ThrRecvSend extends Thread {
 	private static final Logger log = Logger.getLogger(ThrRecvSend.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private final AbsJoint joint;
+	private final DataInputStream dis;
+	private final DataOutputStream dos;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public ThrRecvSend() {
-		if (flag)
+	public ThrRecvSend(String thrName, AbsJoint joint, InputStream is, OutputStream os) {
+		
+		super(thrName);
+		
+		this.joint = joint;
+		this.dis = new DataInputStream(is);
+		this.dos = new DataOutputStream(os);
+		
+		if (!flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	protected static final int SIZ_DEF_BYTDATA = 4096;
+
+	private LoopSleep loopSleep;
+	
+	private byte[] bytRecv;
+	private int nRecv;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void run() {
+		
+		if (flag) {
+			/*
+			 * initialize
+			 */
+			this.loopSleep = new LoopSleep();
+			
+			this.bytRecv = new byte[SIZ_DEF_BYTDATA];
+			this.nRecv = 0;
+		}
+		
+		if (flag) {
+			
+			try {
+				/*
+				 * start loop
+				 */
+				while (!this.joint.flagStopThread) {
+					if (flag) {
+						/*
+						 * recv
+						 */
+						if (!recv())
+							continue;
+					}
+					
+					if (flag) {
+						/*
+						 * send
+						 */
+						send();
+					}
+					
+					if (flag) {
+						/*
+						 * reset loop sleep time
+						 */
+						loopSleep.reset();
+					}
+				}
+			} catch (Exception e) {
+				if (!flag) e.printStackTrace();
+			} finally {
+				if (flag) {
+					/*
+					 * close and stop thread
+					 */
+					if (this.dis != null) try { this.dis.close(); } catch (IOException e) {}
+					if (this.dos != null) try { this.dos.close(); } catch (IOException e) {}
+					
+					this.joint.flagStopThread = true;
+					
+					if (flag) System.out.printf("[%s] END ...\n", Thread.currentThread().getName());
+				}
+			}
+		}
+	}
+	
+	private boolean recv() throws Exception {
+		
+		try {
+			this.nRecv = this.dis.read(this.bytRecv, 0, SIZ_DEF_BYTDATA);
+			if (this.nRecv < 0) {
+				/*
+				 * EOF -> finish
+				 */
+				if (flag) System.out.printf("%s [EOF] read data of EOF...\n", Thread.currentThread().getName());
+				throw new Exception("read data of EOF. end of stream...");
+			}
+		} catch (SocketTimeoutException e) {
+			// throw e;
+			return false;   // -> continue;
+		} catch (Exception e) {
+			throw e;   // -> finish
+		}
+		
+		return true;
+	}
+
+	private boolean send() throws Exception {
+		
+		try {
+			this.dos.write(this.bytRecv, 0, this.nRecv);
+		} catch (IOException e) {
+			throw e;    // -> finish
+		}
+		
+		return true;
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,9 +193,6 @@ public final class ThrRecvSend extends Thread {
 	 * static test method
 	 */
 	private static void test01(String[] args) throws Exception {
-
-		if (flag)
-			new ThrRecvSend();
 
 		if (flag) {
 

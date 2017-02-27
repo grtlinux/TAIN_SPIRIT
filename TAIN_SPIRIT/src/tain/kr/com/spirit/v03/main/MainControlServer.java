81e881586dd48c1de92b251d96dcce050726a5ff
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-27 13:04:44 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/com/spirit/v03/main/MainControlServer.java
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v03/main/MainControlServer.java
@@ -19,7 +19,15 @@
  */
 package tain.kr.com.spirit.v03.main;
 
+import java.net.ServerSocket;
+
 import org.apache.log4j.Logger;
+
+import tain.kr.com.spirit.v03.control.ThrControlServer;
+import tain.kr.com.spirit.v03.joint.ThrJointServer;
+import tain.kr.com.spirit.v03.loop.LoopSleep;
+import tain.kr.com.spirit.v03.param.ParamContent;
+import tain.kr.com.spirit.v03.queue.QueueContent;
 
 /**
  * Code Templates > Comments > Types
@@ -35,26 +43,129 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class MainControlServer {
+public final class MainControlServer {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(MainControlServer.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String KEY_CONTROL_LISTEN_PORT = "tain.kr.com.spirit.control.listen.port";
+	private static final String KEY_JOINT_LISTEN_PORT = "tain.kr.com.spirit.joint.listen.port";
+	
+	private String controlListenPort;
+	private String jointListenPort;
+	
+	private ServerSocket controlServerSocket;
+	private ServerSocket jointServerSocket;
+
+	private QueueContent queue;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public MainControlServer() {
-		if (flag)
+	public MainControlServer() throws Exception{
+		
+		if (flag) {
+			/*
+			 * ParaContent
+			 */
+			this.controlListenPort = ParamContent.getInstance().getString(KEY_CONTROL_LISTEN_PORT, "20025");
+			this.jointListenPort = ParamContent.getInstance().getString(KEY_JOINT_LISTEN_PORT, "13389");
+		}
+		
+		if (flag) {
+			/*
+			 * initialize
+			 */
+			this.controlServerSocket = new ServerSocket(Integer.parseInt(this.controlListenPort));
+			if (flag) log.debug(String.format(">> control listening port = %s..", this.controlListenPort));
+
+			this.jointServerSocket = new ServerSocket(Integer.parseInt(this.jointListenPort));
+			if (flag) log.debug(String.format(">> joint listening port = %s..", this.jointListenPort));
+
+			this.queue = new QueueContent();
+		}
+		
+		if (flag) {
+			/*
+			 * validation
+			 */
+			if (this.controlServerSocket == null) {
+				throw new Exception("the value of controlServerSocket is null pointer...");
+			}
+
+			if (this.jointServerSocket == null) {
+				throw new Exception("the value of jointServerSocket is null pointer...");
+			}
+
+			if (this.queue == null) {
+				throw new Exception("the value of queue is null pointer...");
+			}
+		}
+		
+		printInfo();
+		
+		if (!flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
+	private void printInfo() {
+		
+		if (flag) System.out.printf("\t ParamContent [%s] = [%s]\n", KEY_CONTROL_LISTEN_PORT, this.controlListenPort);
+		if (flag) System.out.printf("\t ParamContent [%s] = [%s]\n", KEY_JOINT_LISTEN_PORT, this.jointListenPort);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void execute() throws Exception {
+		
+		if (flag) {
+			/*
+			 * begin thread
+			 */
+			
+			if (flag) {
+				/*
+				 * control server thread, maintain connection
+				 */
+				Thread thread = new ThrControlServer(this, this.controlServerSocket);
+				thread.start();
+			}
+			
+			while (true) {
+				/*
+				 * loop of joint server thread
+				 */
+				if (flag) {
+					/*
+					 * joint server thread
+					 */
+					Thread thread = new ThrJointServer(this, this.jointServerSocket);
+					thread.start();
+					thread.join();
+				}
+				
+				if (flag) {
+					/*
+					 * sleep
+					 */
+					LoopSleep.sleep(1 * 500);
+				}
+			}
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public QueueContent getQueue() {
+		return this.queue;
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -71,11 +182,7 @@ public class MainControlServer {
 	private static void test01(String[] args) throws Exception {
 
 		if (flag)
-			new MainControlServer();
-
-		if (flag) {
-
-		}
+			new MainControlServer().execute();
 	}
 
 	/*

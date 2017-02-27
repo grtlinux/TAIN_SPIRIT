dec9a6a62df7f4fb0d7872c4a350ba96769df68e
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-27 13:04:35 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/com/spirit/v03/joint/ThrJointClient.java
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v03/joint/ThrJointClient.java
@@ -19,7 +19,13 @@
  */
 package tain.kr.com.spirit.v03.joint;
 
+import java.io.IOException;
+import java.net.Socket;
+
 import org.apache.log4j.Logger;
+
+import tain.kr.com.spirit.v03.param.ParamContent;
+import tain.kr.com.spirit.v03.recvsend.ThrRecvSend;
 
 /**
  * Code Templates > Comments > Types
@@ -35,26 +41,138 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class ThrJointClient {
+public final class ThrJointClient extends AbsJoint {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(ThrJointClient.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String THREAD_NAME = "JOINT_CLIENT";
+	
+	private static final String KEY_TARGET_HOST = "tain.kr.com.spirit.control.host";
+	private static final String KEY_TARGET_PORT = "tain.kr.com.spirit.control.port";
+
+	private static final String KEY_JOINT_HOST = "tain.kr.com.spirit.control.host";
+	private static final String KEY_JOINT_PORT = "tain.kr.com.spirit.control.port";
+	
+	private final String targetHost;
+	private final String targetPort;
+
+	private final String jointHost;
+	private final String jointPort;
+	
+	private Socket socket1;
+	private Socket socket2;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public ThrJointClient() {
+	public ThrJointClient() throws Exception {
+		
+		super(THREAD_NAME);
+		
+		this.targetHost = ParamContent.getInstance().getString(KEY_TARGET_HOST, "192.168.0.11");
+		this.targetPort = ParamContent.getInstance().getString(KEY_TARGET_PORT, "3389");
+		
+		this.jointHost = ParamContent.getInstance().getString(KEY_JOINT_HOST, "192.168.0.11");
+		this.jointPort = ParamContent.getInstance().getString(KEY_JOINT_PORT, "13389");
+		
+		if (flag) {
+			/*
+			 * socket
+			 */
+			this.socket1 = new Socket(this.targetHost, Integer.parseInt(this.targetPort));
+			this.socket2 = new Socket(this.jointHost, Integer.parseInt(this.jointPort));
+		}
+		
+		if (flag) {
+			/*
+			 * validate
+			 */
+			if (this.socket1 == null) {
+				throw new Exception("the value of socket1 is null pointer...");
+			}
+
+			if (this.socket2 == null) {
+				throw new Exception("the value of socket2 is null pointer...");
+			}
+		}
+		
+		if (flag) {
+			/*
+			 * set options
+			 */
+			this.socket1.setSoTimeout(10 * 1000);
+			this.socket2.setSoTimeout(10 * 1000);
+		}
+
 		if (flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private ThrRecvSend thread1;
+	private ThrRecvSend thread2;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void run() {
+	
+		this.thread1 = null;
+		this.thread2 = null;
+		
+		if (flag) {
+			/*
+			 * create thread
+			 */
+			try {
+				this.thread1 = new ThrRecvSend(String.format("CONTROL_CLIENT_RECVSEND_01"), this
+						, this.socket1.getInputStream(), this.socket2.getOutputStream());
+
+				this.thread2 = new ThrRecvSend(String.format("CONTROL_CLIENT_RECVSEND_02"), this
+						, this.socket2.getInputStream(), this.socket1.getOutputStream());
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+		
+		if (flag) {
+			/*
+			 * start thread
+			 */
+			this.thread1.start();
+			this.thread2.start();
+		}
+		
+		if (flag) {
+			/*
+			 * join thread
+			 */
+			try {
+				this.thread1.join();
+				this.thread2.join();
+			} catch (InterruptedException e) {}
+		}
+		
+		if (flag) {
+			/*
+			 * close
+			 */
+			if (this.socket1 != null) try { this.socket1.close(); } catch (IOException e) {}
+			if (this.socket2 != null) try { this.socket2.close(); } catch (IOException e) {}
+			
+			if (flag) System.out.printf("[%s] END ...\n", Thread.currentThread().getName());
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////

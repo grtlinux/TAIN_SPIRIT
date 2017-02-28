3090487b8305bf805701c10d92016f982ee37e2e
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-28 09:19:31 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/runjar/v04/TestMainRunJar.java
+++ TAIN_SPIRIT/src/tain/kr/runjar/v04/TestMainRunJar.java
@@ -19,6 +19,8 @@
  */
 package tain.kr.runjar.v04;
 
+import java.lang.reflect.Method;
+import java.util.MissingResourceException;
 import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -39,7 +41,7 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class TestMainRunJar {
+public final class TestMainRunJar {
 
 	private static boolean flag = true;
 
@@ -47,11 +49,8 @@ public class TestMainRunJar {
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
-	private static final String KEY_SPIRIT_VERSION = "tain.kr.com.spirit.version";
-	private static final String KEY_SPIRIT_DESC = "tain.kr.com.spirit.desc";
-
-	private String version;
-	private String desc;
+	private final Properties prop;
+	private final ResourceBundle resourceBundle;
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -60,19 +59,10 @@ public class TestMainRunJar {
 	 */
 	public TestMainRunJar() {
 		
-		if (flag) {
-			/*
-			 * ResourceBundle
-			 */
-			if (flag) System.out.printf("\n########## ResourceBundle\n");
+		this.prop = System.getProperties();
+		this.resourceBundle = ResourceBundle.getBundle(this.getClass().getName().replace('.', '/'));
 
-			ResourceBundle rb = ResourceBundle.getBundle(this.getClass().getName().replace('.', '/'));
-			
-			this.version = rb.getString(KEY_SPIRIT_VERSION);
-			this.desc = rb.getString(KEY_SPIRIT_DESC);
-			
-			if (flag) log.debug(String.format("ResourceBundle => [version=%s] [desc=%s]", this.version, this.desc));
-		}
+		printInformation();
 
 		if (flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
@@ -80,7 +70,90 @@ public class TestMainRunJar {
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String KEY_INFORMATION = "tain.kr.com.spirit.information";
+	private static final String KEY_PROJECT = "tain.kr.com.spirit.project";
+	private static final String KEY_COMPANY = "tain.kr.com.spirit.company";
+	private static final String KEY_AUTHOR = "tain.kr.com.spirit.author";
+	private static final String KEY_VERSION = "tain.kr.com.spirit.version";
+	private static final String KEY_SITE = "tain.kr.com.spirit.site";
+	private static final String KEY_DESC = "tain.kr.com.spirit.desc";
+	private static final String KEY_COPYRIGHT = "tain.kr.com.spirit.copyright";
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private void printInformation() {
+		
+		if (flag) {
+			if (flag) System.out.printf("\t----------------------------------------------------\n");
+			if (flag) System.out.printf("\t[%s] = [%s]\n", KEY_INFORMATION, this.resourceBundle.getString(KEY_INFORMATION));
+			if (flag) System.out.printf("\t[%s] = [%s]\n", KEY_PROJECT, this.resourceBundle.getString(KEY_PROJECT));
+			if (flag) System.out.printf("\t[%s] = [%s]\n", KEY_COMPANY, this.resourceBundle.getString(KEY_COMPANY));
+			if (flag) System.out.printf("\t[%s] = [%s]\n", KEY_AUTHOR, this.resourceBundle.getString(KEY_AUTHOR));
+			if (flag) System.out.printf("\t[%s] = [%s]\n", KEY_VERSION, this.resourceBundle.getString(KEY_VERSION));
+			if (flag) System.out.printf("\t[%s] = [%s]\n", KEY_SITE, this.resourceBundle.getString(KEY_SITE));
+			if (flag) System.out.printf("\t[%s] = [%s]\n", KEY_DESC, this.resourceBundle.getString(KEY_DESC));
+			if (flag) System.out.printf("\t[%s] = [%s]\n", KEY_COPYRIGHT, this.resourceBundle.getString(KEY_COPYRIGHT));
+			if (flag) System.out.printf("\t----------------------------------------------------\n");
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private String getString(String strKey) {
+		
+		String strValue = null;
+		
+		strValue = this.prop.getProperty(strKey);
+		if (strValue != null)
+			return strValue;
+		
+		try {
+			strValue = this.resourceBundle.getString(strKey);
+		} catch (MissingResourceException e) {}
+		
+		return strValue;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static final String KEY_CLASS = "class";
+
+	private String strType;
+	private String strClass;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public void execute() throws Exception {
+		
+		this.strType = getString(KEY_CLASS);
+		if (this.strType == null) {
+			throw new Exception("ERROR: there's no class name.. -Dclass=[SpiritServer/SpiritClient/TestServer/TestClient].");
+		}
+		
+		this.strClass = getString(this.strType);
+		
+		if (this.strClass == null) {
+			throw new Exception("ERROR: there's wrong class name.. -Dclass=[SpiritServer/SpiritClient/TestServer/TestClient].(strType=" + this.strType + ")");
+		}
+		
+		runSpirit(this.strClass);
+	}
+	
+	private void runSpirit(String strClass) throws Exception {
+		
+		if (flag) {
+			if (flag) System.out.printf("class=[%s]\n", strClass);
+			
+			Class<?> cls = Class.forName(strClass);
+			
+			Method main = cls.getDeclaredMethod("main", new Class[] { String[].class });
+			
+			main.invoke(null, (Object) new String[] { "Hello", "World!!!" });
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -160,6 +233,14 @@ public class TestMainRunJar {
 	}
 
 	/*
+	 * static test method
+	 */
+	private static void test02(String[] args) throws Exception {
+		
+		if (flag) new TestMainRunJar().execute();
+	}
+	
+	/*
 	 * main method
 	 */
 	public static void main(String[] args) throws Exception {
@@ -168,7 +249,8 @@ public class TestMainRunJar {
 			log.debug(">>>>> " + new Object() {
 			}.getClass().getEnclosingClass().getName());
 
-		if (flag)
-			test01(args);
+		if (!flag) test01(args);
+		
+		if (flag) test02(args);
 	}
 }

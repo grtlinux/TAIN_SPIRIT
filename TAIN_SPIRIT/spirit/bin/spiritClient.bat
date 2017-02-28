366070ee19156b35a3665d1197b7dadf521b051f
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-01 08:09:03 +0900

    .

--- TAIN_SPIRIT/spirit/bin/spiritClient.bat
+++ TAIN_SPIRIT/spirit/bin/spiritClient.bat
@@ -5,7 +5,8 @@
 :ENV
 
 set JAVA_HOME=N:\PROG\jdk1.7.0_79
-::set JAVA_HOME=..\..\jdk1.7.0_79
+if not exist %JAVA_HOME% set JAVA_HOME=..\..\jdk1.7.0_79
+
 set PATH=%PATH%;%JAVA_HOME%\bin
 
 set JAR_NAME=tain-spirit-1.0.jar
@@ -14,12 +15,11 @@ set JAR_NAME=tain-spirit-1.0.jar
 :OPTION
 
 set OPTION= -Dclass=SpiritClient
-set OPTION=%OPTION% -Dtain.kr.com.spirit.control.host=localhost
-set OPTION=%OPTION% -Dtain.kr.com.spirit.control.port=20025
-set OPTION=%OPTION% -Dtain.kr.com.spirit.control.retry=1
+
+set OPTION=%OPTION% -Dtain.kr.com.spirit.joint.retry=5
 
 set OPTION=%OPTION% -Dtain.kr.com.spirit.joint.host=localhost
-set OPTION=%OPTION% -Dtain.kr.com.spirit.joint.port=20026
+set OPTION=%OPTION% -Dtain.kr.com.spirit.joint.port=20025
 
 set OPTION=%OPTION% -Dtain.kr.com.spirit.target.host=localhost
 set OPTION=%OPTION% -Dtain.kr.com.spirit.target.port=13389

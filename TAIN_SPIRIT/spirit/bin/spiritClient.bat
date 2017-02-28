079068b1e31de8b3620b6003df1e4d8997e03bbd
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-28 09:59:14 +0900

    .

---
+++ TAIN_SPIRIT/spirit/bin/spiritClient.bat
@@ -0,0 +1,29 @@
+@echo on
+@setlocal
+
+::-----------------------------------------------------------------------------------------
+:ENV
+
+set JAVA_HOME=N:\PROG\jdk1.7.0_79
+set PATH=%PATH%;%JAVA_HOME%\bin
+
+set JAR_NAME=tain-spirit-1.0.jar
+
+::-----------------------------------------------------------------------------------------
+:OPTION
+
+set OPTION= -Dclass=TestClient
+set OPTION=%OPTION% -Dtain.kr.com.spirit.test.client.host=192.168.0.11
+set OPTION=%OPTION% -Dtain.kr.com.spirit.test.client.port=13389
+
+::-----------------------------------------------------------------------------------------
+:RUN_JAR
+
+java %OPTION% -jar %JAR_NAME%
+
+::-----------------------------------------------------------------------------------------
+:END
+
+pause
+::timeout 1
+

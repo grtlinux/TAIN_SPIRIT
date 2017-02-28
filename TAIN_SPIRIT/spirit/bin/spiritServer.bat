201840363532d2e923ba26c66724879499cca25b
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-01 08:09:06 +0900

    .

--- TAIN_SPIRIT/spirit/bin/spiritServer.bat
+++ TAIN_SPIRIT/spirit/bin/spiritServer.bat
@@ -5,7 +5,8 @@
 :ENV
 
 set JAVA_HOME=N:\PROG\jdk1.7.0_79
-::set JAVA_HOME=..\..\jdk1.7.0_79
+if not exist %JAVA_HOME% set JAVA_HOME=..\..\jdk1.7.0_79
+
 set PATH=%PATH%;%JAVA_HOME%\bin
 
 set JAR_NAME=tain-spirit-1.0.jar
@@ -14,8 +15,7 @@ set JAR_NAME=tain-spirit-1.0.jar
 :OPTION
 
 set OPTION= -Dclass=SpiritServer
-set OPTION=%OPTION% -Dtain.kr.com.spirit.control.listen.port=20025
-set OPTION=%OPTION% -Dtain.kr.com.spirit.joint.listen.port=20026
+set OPTION=%OPTION% -Dtain.kr.com.spirit.joint.listen.port=20025
 
 ::-----------------------------------------------------------------------------------------
 :RUN_JAR

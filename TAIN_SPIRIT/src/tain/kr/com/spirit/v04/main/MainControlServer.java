c8fa0e59b26b530bb01fa1f39088b3feff1c6b54
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-01 09:12:30 +0900

    .

--- TAIN_SPIRIT/src/tain/kr/com/spirit/v04/main/MainControlServer.java
+++ TAIN_SPIRIT/src/tain/kr/com/spirit/v04/main/MainControlServer.java
@@ -99,7 +99,7 @@ public final class MainControlServer {
 						thread.start();
 						thread.join();
 					} catch (Exception e) {
-						if (flag) System.out.printf("\tMainControlServer %s - %s\n", e, Utils.getInstance().getDateTime());
+						if (flag) System.out.printf("\t##### %s - %s\n", e, Utils.getInstance().getDateTime());
 						//e.printStackTrace();
 					}
 				}

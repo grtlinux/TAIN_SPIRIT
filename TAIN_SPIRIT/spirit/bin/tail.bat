3d422f1d432ca400aaadad267b526afceeb6a4ac
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-02 01:36:23 +0900

    .

---
+++ TAIN_SPIRIT/spirit/bin/tail.bat
@@ -0,0 +1,28 @@
+@echo on
+
+setlocal
+
+::----------------------------------------------
+:: ENV
+
+::set TAIN_HOME=N:\PROG
+set TAIN_HOME=D:\IMSI\SPIRIT_20170302
+if not exist %TAIN_HOME% set TAIN_HOME=..\..
+
+set JAVA_HOME=%TAIN_HOME%\jdk1.7.0_79
+set SPIRIT_HOME=%TAIN_HOME%\sprit
+
+
+set PATH=%JAVA_HOME%\bin;%SPIRIT_HOME%\bin;%SPIRIT_HOME%\bin\UnxUpdates;%SPIRIT_HOME%\bin\nssm-2.24\win64;%PATH%
+
+cd %SPIRIT_HOME%\bin
+
+::----------------------------------------------
+:: START
+
+tain -f ..\log\spirit_out.log
+
+
+
+::endlocal
+

52214a00fc73ffac2c8e3f3c4dad48a6822df745
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-02 01:10:05 +0900

    .

--- TAIN_SPIRIT/spirit/bin/___dos.bat
+++ TAIN_SPIRIT/spirit/bin/___dos.bat
@@ -5,6 +5,8 @@ setlocal
 ::----------------------------------------------
 :: ENV
 set JAVA_HOME=N:\PROG\jdk1.7.0_79
+if not exist %JAVA_HOME% set JAVA_HOME=..\..\jdk1.7.0_79
+
 set CATALINA=N:\PROG\apache-tomcat-7.0.68-x64
 
 set PATH=%JAVA_HOME%\bin;%CATALINA%\bin;%PATH%

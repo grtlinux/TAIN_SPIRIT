d15ccdb9394cb73a3056fd2945a61dd7dc0b336d
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-02 02:05:47 +0900

    .

--- TAIN_SPIRIT/spirit/bin/___dos.bat
+++ TAIN_SPIRIT/spirit/bin/___dos.bat
@@ -9,7 +9,7 @@ set TAIN_HOME=D:\IMSI\SPIRIT_20170302
 if not exist %TAIN_HOME% set TAIN_HOME=..\..
 
 set JAVA_HOME=%TAIN_HOME%\jdk1.7.0_79
-set SPIRIT_HOME=%TAIN_HOME%\sprit
+set SPIRIT_HOME=%TAIN_HOME%\spirit
 
 
 set PATH=%JAVA_HOME%\bin;%SPIRIT_HOME%\bin;%SPIRIT_HOME%\bin\UnxUpdates;%SPIRIT_HOME%\bin\nssm-2.24\win64;%PATH%
